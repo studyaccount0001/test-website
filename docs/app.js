a73cc4e113874437b9b25ba837f1423b9d376598
studyaccount0001 <79167129+studyaccount0001@users.noreply.github.com>
2022-05-10 22:47:24 -0300

    finally

--- docs/app.js
+++ docs/app.js
@@ -2,3 +2,13 @@
 particlesJS.load("particles-js", "particlesjs-config.json", function () {
     console.log("callback - particles.js config loaded");
 });
+
+function scrollTo(hash) {
+    const element = document.getElementsByName(`#${hash}`);
+    const topPos = element.getBoundingClientRect().top + window.pageYOffset;
+
+    window.scrollTo({
+        top: topPos, // scroll so that the element is at the top of the view
+        behavior: "smooth", // smooth scroll
+    });
+}
